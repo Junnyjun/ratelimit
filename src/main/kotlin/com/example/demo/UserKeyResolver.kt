5cf51545c4e4d930a3cba3137801b6b80f432654
Junnyjun <chbe5082@naver.com>
2024-09-04 23:10:04 +0900

    spring rector

--- src/main/kotlin/com/example/demo/UserKeyResolver.kt
+++ src/main/kotlin/com/example/demo/UserKeyResolver.kt
@@ -1,4 +1,19 @@
 package com.example.demo
 
-import org.springframework.stereotype.Component
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import reactor.core.publisher.Mono
+
+
+@Configuration
+class CustomUserKeyResolver {
+    @Bean
+    fun userKeyResolver(): KeyResolver {
+        return KeyResolver { exchange ->
+            Mono.just(exchange.request.queryParams.getFirst("user").toString())
+        }
+    }
+}
+
 
